97504573620f8ca251f25f7db99f262011a7c61a
Paulo Roberto Urio <paulourio@gmail.com>
2019-05-18 19:58:26 -0300

    Review metrics
    Signed-off-by: Paulo Roberto Urio <paulourio@gmail.com>

--- war/metrics.py
+++ war/metrics.py
@@ -1,10 +1,12 @@
+"""Metrics for task evaluation."""
+# pylint: disable=C0103
 from sklearn import metrics
 
 
 def gini_score(expected, predicted):
+    """Compute the Gini normalized score."""
     auc = metrics.roc_auc_score(expected, predicted[:, 1])
     return (auc - 0.5) * 2
 
 
 gini = metrics.make_scorer(gini_score, needs_proba=True)
-
